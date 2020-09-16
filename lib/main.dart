013fb2200738dae7d0de8b5ed646dc8d54261d1f
rafaleal <rafaelalealmartinss@gmail.com>
2020-09-16 18:27:42 -0300

    original background color

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Calculadora de IMC'),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
