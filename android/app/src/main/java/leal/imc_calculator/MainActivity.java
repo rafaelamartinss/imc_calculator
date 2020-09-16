dcbcf0f6c5ce326765dba43bfb39b0c9b423ddde
rafaleal <rafaelalealmartinss@gmai.com>
2020-09-16 18:23:41 -0300

    basic app to calculate imc

---
+++ android/app/src/main/java/leal/imc_calculator/MainActivity.java
@@ -0,0 +1,6 @@
+package leal.imc_calculator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = 'Informe seus dados!';
+
+  void _resetFields() {
+      weightController.text = "";
+      heightController.text = "";
+      setState(() {
+        _infoText = "Informe seus dados!!!";
+        _formKey = GlobalKey<FormState>();
+      });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      if(imc < 18.6){
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _resetFields,
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 100.0, color: Colors.green),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.green, fontSize: 25.0)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 22.0),
+                controller: weightController,
+                validator: (value) {
+                  if(value.isEmpty) {
+                    return "Insira seu peso!";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.green, fontSize: 25.0)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 22.0),
+                controller: heightController,
+                validator: (value) {
+                  if(value.isEmpty) {
+                    return "Insira sua altura!";
+                  }
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
+                child: Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if(_formKey.currentState.validate()) {
+                          _calculate();
+                        }
+                      },
+                      child: Text("Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                      color: Colors.green,
+                    )),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
